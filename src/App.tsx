b406fc93585a5611f418e7bf8971342b78b38782
boris.ayupov <boris.ayupov@gmail.com>
2025-03-17 17:37:21 -0400

    add deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,6 @@ function App() {
           <Route path="/portfolio" element={<Portfolio/>}/>
           <Route path="/contact" element={<ContactUs/>}/>
           <Route path="/get-a-quote" element={<GetAQuote/>}/>
-          {/*<Route path={"/test-area/build-atrium/index.html"} element={<MainPage/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/services/home-staging" element={<HomeStaging/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/services/furnish-and-style" element={<FurnishAndStyle/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/services/vacation-rental" element={<VacationRental/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/portfolio" element={<Portfolio/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/contact" element={<ContactUs/>}/>*/}
-          {/*<Route path="/test-area/build-atrium/get-a-quote" element={<GetAQuote/>}/>*/}
         </Routes>
       <Footer/>
     </div>
